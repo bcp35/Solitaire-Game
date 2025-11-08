64c1ff2e9b0f15f9f6fe52e0586e5fa54ef949b0
bcp35 <bcp35@bath.ac.uk>
2025-11-08 21:34:05 +0000

    Added Card class and GeneratePack() method
    created Card class with constructor and getter methods, as well as a GeneratePack method which creates the standard 52 pack of cards and stores in a numpy array

--- Solitare CLI.py
+++ Solitare CLI.py
@@ -1,3 +1,29 @@
+import numpy as np
+
+class Card:
+    def __init__(self, suit_int, rank_int):
+        self.suit = suits[suit_int]
+        self.rank_int = rank_int + 1
+        self.rank_string = ranks[rank_int]
+        self.rank_fullname = ranks_fullname[rank_int]
+        self.colour = colours[suit_int // 2]
+    def getSuit(self):
+        return self.suit
+    def getRank(self):
+        return self.rank_int
+    def getColour(self):
+        return self.colour
+    def getDisplayName(self):
+        return self.suit[0] + self.rank_string
+    def getFullName(self):
+        return self.rank_fullname + " of " + self.suit
+
+
+suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+ranks = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+ranks_fullname = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+colours = ["Red", "Black"]
+
 def MainMenu():
     print("Welcome to Solitare!")
     print("Enter the number of the option you'd like to select:")
@@ -11,12 +37,20 @@ def MainMenu():
         Instructions()
     elif x == 3:
         exit()
-    
-def GameSetup():
-    print("Shuffling deck...")
 
 def Instructions():
     print("How to play Solitare:")
+ 
+def GeneratePack():
+    pack = np.empty(52, dtype = object)
+    for i in range(4):
+        for j in range(13):
+            pack[13*i + j] = Card(i, j)
+    return pack
+
+def GameSetup():
+    pack = GeneratePack()
+    print("Shuffling deck...")
 
 
 MainMenu()
