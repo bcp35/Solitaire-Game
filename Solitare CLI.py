87962ff074ae2e8fac9ddc0592c8b20f7a8605cc
bcp35 <bcp35@bath.ac.uk>
2025-11-08 20:44:29 +0000

    Added MainMenu()
    Created a main menu method that gives the user the option to start a game, view the instructions or exit the application

--- Solitare CLI.py
+++ Solitare CLI.py
@@ -1,1 +1,22 @@
-print("Welcome to Solitare!")
+def MainMenu():
+    print("Welcome to Solitare!")
+    print("Enter the number of the option you'd like to select:")
+    print(" 1. Start Game")
+    print(" 2. Instructions")
+    print(" 3. Exit")
+    x = int(input("Enter a number: "))
+    if x == 1:
+        GameSetup()
+    elif x == 2:
+        Instructions()
+    elif x == 3:
+        exit()
+    
+def GameSetup():
+    print("Shuffling deck...")
+
+def Instructions():
+    print("How to play Solitare:")
+
+
+MainMenu()
